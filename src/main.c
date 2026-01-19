5d1b1a4e97379b5bdf647fec86f62ed89ccc2a29
irrevuel <irene.revueltagomez@gmail.com>
2026-01-19 21:14:00 +0100

    merge

--- src/main.c
+++ src/main.c
@@ -21,20 +21,17 @@ int main(int argc, char *argv[], char *envp[])
 	(void)envp;
     char *line;
     t_exec_ctx ctx;
-    int i;
 
     ctx.envp = envp;
     ctx.last_status = 0;
     ctx.interactive = 1;
-    i = 0;
     while (1) // tengo que ver cómo gestionar esto
     {
         line = readline("minishell-1.0 $ ");
         add_history(line);       // se recupera solo el historial
         handle_input(line, ctx); // funcion handdle input -> lexer -> tokens -> parser -> llamo funcion de nico
         free(line);
-        i++;
     }
-    clear_history(); // rl_clear_history en Linux
+    //clear_history(); // rl_clear_history en Linux
     return 0;
 }
